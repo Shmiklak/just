b2545ed291deed4ebacb077ea91cd92d384b3d9c
Shmiklak <shmiklak@gmail.com>
2020-02-01 21:46:39 +0500

    Fixed main-view bug

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -40,7 +40,9 @@ if (isAndroid) {
 }
 
 // Initialize app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    swipePanel: 'left'
+});
 
 // Add view
 var mainView = myApp.addView('.view-main', {
@@ -49,43 +51,14 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Handle Cordova Device Ready Event
-// $$(document).on('deviceready', function() {
-//     mainView.router.load({
-//         url: 'login.html',
-//         options: {
-//             transition: 'f7-fade'
-//         }
-//     });
-// });
-//
+$$(document).on('deviceready', function() {
 
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
 });
+
+
 
 $$(document).on('click', '#login-button', function(e) {
     mainView.router.load({
         url: 'tablesheet.html'
     });
-})
+});
